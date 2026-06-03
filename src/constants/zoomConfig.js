ab685cd43b70fea2d604c3e040f061da1deedf8b
naseemudheen <naseemudheene@gmail.com>
2026-06-03 22:34:14 +0530

    chore: update DIRECTION_INITIAL_ZOOM level from 3 to 2

--- src/constants/zoomConfig.js
+++ src/constants/zoomConfig.js
@@ -1,3 +1,3 @@
 export const NAVIGATION_ZOOM_LEVEL = 3;
 export const FLOORPLAN_INITIAL_ZOOM = 0.5; // Used in Floorplan.jsx (Navigation)
-export const DIRECTION_INITIAL_ZOOM = 3; // Used in DirectionFloor.jsx (Directions)
+export const DIRECTION_INITIAL_ZOOM = 2; // Used in DirectionFloor.jsx (Directions)
